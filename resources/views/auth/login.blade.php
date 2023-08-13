8396028f92f589bc44710a83dc3885a75da47f77
Ibrahim Salah <57736764+IbrahimSalahEldin@users.noreply.github.com>
2023-08-13 13:39:05 -1000

    add features

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" dir="rtl">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -107,7 +107,8 @@
     <div class="row justify-content-center">
         <div class="col-md-8 cardOfLogin">
             <div class="card ">
-                <div class="card-header">{{ __('Login') }}</div>
+
+            <div class="card-header">تسجيل الدخول</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
